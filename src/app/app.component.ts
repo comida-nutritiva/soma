40d8b0825405caafce3934e5b199e28bcfd80d7e
Alan personal <alany.diaz97@gmail.com>
2025-11-12 18:00:43 -0300

    Fixed one ID for user/browser

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject, PLATFORM_ID } from '@angular/core';
 import { HeadComponent } from './head/head.component';
 import { ProductListComponent } from "./product-list/product-list.component";
 import { NutritionalFactComponent } from './nutritional-fact/nutritional-fact.component';
 import { PerOrderComponent } from './per-order/per-order.component';
 import { NavComponent } from './nav/nav.component';
+import { isPlatformBrowser } from '@angular/common';
+import { getAnalytics, setUserId } from 'firebase/analytics';
 
 @Component({
   selector: 'app-root',
@@ -17,6 +19,25 @@ import { NavComponent } from './nav/nav.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+
+
+
+export class AppComponent  implements OnInit {
   title = 'soma';
+
+  private platformId = inject(PLATFORM_ID);
+
+  ngOnInit() {
+    //creacion de ID por navegador en client side rendering
+    if (!isPlatformBrowser(this.platformId)) return;
+
+    let userId = localStorage.getItem('user_id');
+    if (!userId) {
+      userId = crypto.randomUUID(); 
+      localStorage.setItem('user_id', userId);
+    }
+
+    const analytics = getAnalytics();
+    setUserId(analytics, userId);
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics, setUserId } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from 'firebase/app';
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDN--mJidlaG0Xez36eC57ZM64hpqgh1LQ",
   authDomain: "soma16-8.firebaseapp.com",
@@ -21,20 +15,8 @@ const firebaseConfig = {
   measurementId: "G-9D88Q1FZRT"
 };
 
-
-// generar o recuperar el userId
-let userId = localStorage.getItem('userId');
-if (!userId) {
-  userId = crypto.randomUUID(); // genera un ID único
-  localStorage.setItem('userId', userId);
-}
-
-
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
-setUserId(analytics, userId);
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
